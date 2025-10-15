3a053293742246f696bc34eda3c46160836f24e1
Sebastianmedellin <smedellin@estudiante.uniajc.edu.co>
2025-10-15 17:58:46 -0500

    Se realiza la clase guerrero

--- src/main/java/com/uniajc/Guerrero.java
+++ src/main/java/com/uniajc/Guerrero.java
@@ -1,5 +1,23 @@
 package com.uniajc;
 
-public class Guerrero {
- 
+public class Guerrero extends Criatura {
+    //metodo constructor
+    public Guerrero(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+    @Override
+    // Implementación del método atacar
+    public void atacar(Criatura objetivo) {
+        int daño = this.fuerza;
+        if (this.arma != null) daño += this.arma.getDañoAdicional();
+        objetivo.defender(daño);
+        System.out.println(this.nombre + " ataca a " + objetivo.nombre + " causando " + daño + " de daño.");
+    }
+    @Override
+    // Implementación del método defender
+    public void defender(int daño) {
+        this.salud -= daño;
+        System.out.println(this.nombre + " recibe " + daño + " de daño. Salud restante: " + this.salud);
+    }
+    
 }
